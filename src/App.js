ca86f8b216c5e4d8d6859b9542be0f03c083eaf1
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-01-16 13:38:24 -0500

    Cleaned up App.js further

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
 import Home from "./components/home";
 import Setup from "./components/setup";
 import Commands from "./components/commands";
@@ -8,14 +7,12 @@ import Other from "./components/other";
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/setup" component={Setup} />
-        <Route path="/commands" component={Commands} />
-        <Route component={Other} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/setup" component={Setup} />
+      <Route path="/commands" component={Commands} />
+      <Route component={Other} />
+    </Switch>
   );
 }
 
